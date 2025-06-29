30c7ddf3aa1e072e208d1573a61a8295758336ef
Tyson Zimmer <zimmertyson@gmail.com>
2025-06-28 21:28:01 -0700

    Add expanded code for the find pokemon app code

--- find-random-pokemon/script.js
+++ find-random-pokemon/script.js
@@ -1,7 +1,10 @@
-async function getPokemon (pokemonName) {
+async function getPokemon(pokemonName) {
   try {
-    if(typeof pokemonName !== "string" || !pokemonName) {
-      throw new Error ("Messed up");
+    if (typeof pokemonName !== "string" || !pokemonName) {
+      throw new Error("Error");
+      const response = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${pokemonName.toLowerCase()}`
+      );
     }
   } catch {
 
