7b73af5a70fe4d5f31c5182ee842df7f5e706488
Tyson Zimmer <zimmertyson@gmail.com>
2025-06-30 10:59:58 -0700

    Continued updated for the find random pokemon js project

--- find-random-pokemon/script.js
+++ find-random-pokemon/script.js
@@ -5,6 +5,22 @@ async function getPokemon(pokemonName) {
       const response = await fetch(
         `https://pokeapi.co/api/v2/pokemon/${pokemonName.toLowerCase()}`
       );
+      if(!response.ok) throw new Error(response.status);
+      const pokemon = await response.JSON()
+      console.log(pokemon);
+      const name = pokemon.name;
+      const height = pokemon.height;
+      const weight = pokemon.weight;
+      const image = pokemon.sprites.front_default;
+
+      const types = pokemon.types.map((element) => element.type.name);
+      const sound = Object.entries(pokemon.cries).map(([key, val]) => {
+        return {
+          soundType:key,
+          sound:val,
+        };
+      });
+
     }
   } catch {
 
