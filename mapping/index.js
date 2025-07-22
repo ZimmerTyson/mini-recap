43f1e9aa711ebf959596ecf8eae8f706444656bf
Tyson Zimmer <zimmertyson@gmail.com>
2025-07-21 21:10:04 -0700

    new problem solved for the day

---
+++ mapping/index.js
@@ -0,0 +1,61 @@
+// Task 1: Create an array of numbers
+// Task 2: Use map to return a new array with each number doubled
+// Task 3: Console log the new array
+
+const numbers = [2, 4, 6];
+const doubled = numbers.map((n) => n * 2);
+console.log(doubled);
+
+// --- 10 Practice Tasks ---
+// Example 1: Double each number in an array using map
+
+const number = [20, 40, 77];
+const double = number.map((n) => n * 2);
+
+console.log(double);
+
+// Example 2: Convert each word to uppercase using map
+
+const words = ['hello', 'howdy', 'gday'];
+// const uppercase = words.map((upper) => upper.toUpperCase());
+
+// console.log(uppercase);
+
+// Another way to do number 2: 
+
+console.log(words.map((animals) => animals.toUpperCase()));
+
+// Example 3: Add 1 to each number using
+
+const numbersArr = [1, 2, 3, 4];
+console.log(numbersArr.map((num) => num + 1));
+
+// Example 4: Square each number in array using map
+
+const squareNum = [2, 4, 5, 6];
+console.log(squareNum.map((num) => num * num));
+
+// Example 5: Add exclamation mark to letters using map
+
+const letters = ['i', 'j', 'k'];
+console.log(letters.map((letter) => letter + '!'));
+
+
+// Example 6: Flip each boolean using map
+
+const bool = [true, false, true, true];
+
+console.log(bool.map(function (value) {
+  if(bool.value === true) {
+    
+  }}
+))
+
+// Example 7: Add 'yrs' to each age using map
+
+
+// Example 8: Get length of each name using map
+
+// Example 9: Convert Celsius to Fahrenheit using map
+
+// Example 10: Prefix IDs with 'ID-' using map
