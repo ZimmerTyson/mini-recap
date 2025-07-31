b3f9036e0ee36e4c0c95215d064f1218716858f8
Tyson Zimmer <zimmertyson@gmail.com>
2025-07-31 10:18:11 -0700

    Added a folder containing practice examples on creating fucntions in js.

---
+++ creating-functions/script.js
@@ -0,0 +1,81 @@
+// Task 1: Create a function called greet
+// Task 2: Inside the function, console.log a greeting message
+// Task 3: Call the function below
+
+function greet() {
+  console.log("Hello Will and Colby");
+}
+
+greet();
+
+// --- 10 Practice Tasks ---
+// Example 1: Create a function that logs 'Hello!'
+
+function sayHello() {
+  console.log("Howdy!");
+}
+
+sayHello();
+
+// Example 2: Create a function that logs 'Goodbye!'
+
+function sayGoodbye() {
+  console.log("peace out!");
+}
+
+sayGoodbye();
+// Example 3: Create a function that logs 'Welcome aboard!'
+
+function welcomeAboard() {
+  console.log("Welcome to the team!");
+}
+
+welcomeAboard();
+
+// Example 4: Create a function that logs 'Thanks a lot!'
+
+function thanks() {
+  console.log("Thank you!");
+}
+
+thanks();
+
+// Example 5: Create a function that logs 'You can do it!'
+
+function iBelieve(name) {
+  console.log(`I believe in you, ${name}!`)
+}
+
+iBelieve("Tyson");
+
+// Example 6: Create a function that logs 'Today is a big day!'
+
+function bigDay() {
+  console.log("Today is a big day!");
+}
+
+bigDay();
+
+// Example 8: Create a function that logs 'Notification sent.'
+
+function notificationSent() {
+  console.log("Notification sent!");
+}
+
+notificationSent();
+
+// Example 9: Create a function that logs 'Alert: Action needed.'
+
+function alert() {
+  console.log("Action Needed!");
+}
+
+alert();
+
+// Example 10: Create a function that logs 'All done!'
+
+function allDone() {
+  console.log("All done!");
+}
+
+allDone();
